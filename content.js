b5847296cc9fdc20e15452b6d74d58423e1efd7c
cagostino <cjp.agostino@gmail.com>
2024-07-15 00:27:50 -0400

    updated license and project name to be open web copilot

--- content.js
+++ content.js
@@ -1,20 +1,20 @@
 ///* 
 // * Copyright (C) 2024 NPC Worldwide, LLC
 //*
-//* This file is part of the Google Docs Copilot project.
+//* This file is part of the Open Web Copilot project.
 //*
-//* Google Docs Copilot is free software: you can redistribute it and/or modify
+//* Open Web Copilot is free software: you can redistribute it and/or modify
 //* it under the terms of the GNU Lesser General Public License as published by
 //* the Free Software Foundation, either version 3 of the License, or
 //* (at your option) any later version.
 //*
-//* Google Docs Copilot is distributed in the hope that it will be useful,
+//* Open Web Copilot is distributed in the hope that it will be useful,
 //* but WITHOUT ANY WARRANTY; without even the implied warranty of
 //* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //* GNU Lesser General Public License for more details.
 //*
 //* You should have received a copy of the GNU Lesser General Public License
-//* along with Google Docs Copilot.  If not, see <https://www.gnu.org/licenses/>.
+//* along with Open Web Copilot.  If not, see <https://www.gnu.org/licenses/>.
 //*/
 
 function extractText() {
--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+///* 
+// * Copyright (C) 2024 NPC Worldwide, LLC
+//*
+//* This file is part of the Open Web Copilot project.
+//*
+//* Open Web Copilot is free software: you can redistribute it and/or modify
+//* it under the terms of the GNU Lesser General Public License as published by
+//* the Free Software Foundation, either version 3 of the License, or
+//* (at your option) any later version.
+//*
+//* Open Web Copilot is distributed in the hope that it will be useful,
+//* but WITHOUT ANY WARRANTY; without even the implied warranty of
+//* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//* GNU Lesser General Public License for more details.
+//*
+//* You should have received a copy of the GNU Lesser General Public License
+//* along with Open Web Copilot.  If not, see <https://www.gnu.org/licenses/>.
+//*/
 import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
