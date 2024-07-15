667a178540788bd1ed80bf7968362e67fbbb9bc1
cagostino <cjp.agostino@gmail.com>
2024-07-14 23:37:54 -0400

    tested with qwen and phi and deepseekcoder. success! adjusted the prompt in server.js a bit to deal with the weird formatting,

--- server.js
+++ server.js
@@ -1,49 +1,41 @@
-///* 
-// * Copyright (C) 2024 NPC Worldwide, LLC
-//*
-//* This file is part of the Google Docs Copilot project.
-//*
-//* Google Docs Copilot is free software: you can redistribute it and/or modify
-//* it under the terms of the GNU Lesser General Public License as published by
-//* the Free Software Foundation, either version 3 of the License, or
-//* (at your option) any later version.
-//*
-//* Google Docs Copilot is distributed in the hope that it will be useful,
-//* but WITHOUT ANY WARRANTY; without even the implied warranty of
-//* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//* GNU Lesser General Public License for more details.
-//*
-//* You should have received a copy of the GNU Lesser General Public License
-//* along with Google Docs Copilot.  If not, see <https://www.gnu.org/licenses/>.
-//*/
-
 import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+console.log('Starting server...');
+
+dotenv.config();
+console.log('Environment variables loaded.');
 
 const app = express();
 const port = 3333;
 
+console.log('Express app created.');
+
 app.use(cors());
 app.use(express.json());
 
+console.log('Middleware set up.');
+
 app.post('/generate', async (req, res) => {
+  console.log('Received request to /generate');
   const { context } = req.body;
   console.log('Received context:', context);
 
   try {
+    console.log('Sending request to Ollama...');
     const ollamaResponse = await fetch('http://localhost:11434/api/generate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        model: process.env.MODEL_NAME, // Use the environment variable
-        prompt: 'Please finish the following item: "" ' + context + ' "".  Do not provide any additional information. Reply with only the text that should be added to complete the item.',
+        model: process.env.MODEL_NAME,
+        prompt: 'What will follow on a new line is a piece of text that you must continue.   Do not provide any additional information. Reply with only the text that you generate that continues the text. Content: \n ' +   context + '. ',
         stream: false
       })
     });
 
+    console.log('Received response from Ollama');
     const data = await ollamaResponse.json();
     console.log('Generated text:', data.response);
     res.json({ generatedText: data.response });
@@ -52,11 +44,13 @@ app.post('/generate', async (req, res) => {
     res.status(500).json({ error: 'Failed to generate text' });
   }
 });
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
